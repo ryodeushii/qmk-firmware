8c6b5d1793b0d24c564d50f63f34bbfaa64ab697
Patrick Braun <sonofmoghisthebest@gmx.de>
2025-03-09 13:53:08 +0100

    refactor: rename ansi.c for reuse in iso port

--- keyboards/nuphy/gem80/ansi/rules.mk
+++ keyboards/nuphy/gem80/ansi/rules.mk
@@ -5,6 +5,8 @@ SRC += side.c side_driver.c side_logo.c
 # can be separated with needed fields implemented in ansi.h
 SRC += mcu_pwr.c sleep.c debounce.c rf_driver.c rf_queue.c
 
+SRC += gem80-common.c
+
 UART_DRIVER_REQUIRED = yes
 
 OPT ?= 2
